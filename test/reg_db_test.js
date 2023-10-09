fb27e4561fc170fe42b941d3450428285da4afba
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-10-09 13:41:07 +0200

    added tests

--- test/reg_db_test.js
+++ test/reg_db_test.js
@@ -27,30 +27,52 @@ describe("Registration webapp", async function () {
 
 
     describe("addRegNum", async function () {
+
         it("should be able to add registration numbers", async function () {
             const registration = RegistrationDb(db);
-            const regNum = "CA 3443";
-            await registration.addRegNum(regNum);
-            assert.deepEqual(await registration.getRegNums(),[{ reg_number: 'CA 3443' }]);
+            await registration.addRegNum("CA 3443");
+            assert.deepEqual(await registration.getRegNums(), [{ reg_number: 'CA 3443' }]);
         });
+
+        it("should be able to add registration numbers", async function () {
+            const registration = RegistrationDb(db);
+            await registration.addRegNum("CF 279-563");
+            assert.deepEqual(await registration.getRegNums(), [{ reg_number: 'CF 279-563' }]);
+        });
+
+        it("should be able to add registration numbers", async function () {
+            const registration = RegistrationDb(db);
+            await registration.addRegNum("CA 250 963");
+            assert.deepEqual(await registration.getRegNums(), [{ reg_number: 'CA 250 963' }]);
+        });
+
 
         it("should not be able to add registration numbers that are not from CA,CF,CL,CJ ", async function () {
             const registration = RegistrationDb(db);
             const regNum = "CEF 3443";
             await registration.addRegNum(regNum);
-            assert.deepEqual(await registration.getRegNums(),[]);
+            assert.deepEqual(await registration.getRegNums(), []);
         });
+
+        it("should not be able to add registration numbers that are not from CA,CF,CL,CJ ", async function () {
+            const registration = RegistrationDb(db);
+            const regNum = "CFM 346-574";
+            await registration.addRegNum(regNum);
+            assert.deepEqual(await registration.getRegNums(), []);
+        });
+        
+
         it("should not add registration numbers with characters greater than 11", async function () {
             const registration = RegistrationDb(db);
             const regNum = "CEF 34877455543";
             await registration.addRegNum(regNum);
-            assert.deepEqual(await registration.getRegNums(),[]);
+            assert.deepEqual(await registration.getRegNums(), []);
         });
         it("should not add registration numbers with that have special charecters", async function () {
             const registration = RegistrationDb(db);
             const regNum = "CE @5543";
             await registration.addRegNum(regNum);
-            assert.deepEqual(await registration.getRegNums(),[]);
+            assert.deepEqual(await registration.getRegNums(), []);
         });
     });
 
@@ -61,7 +83,7 @@ describe("Registration webapp", async function () {
             await registration.addRegNum("CA 745-564");
             await registration.addRegNum("CA 745-564");
             await registration.addRegNum("CA 3443");
-            assert.deepEqual(await registration.getRegNums(),[{ reg_number: 'CA 745-564' }, { reg_number: 'CA 3443' }])
+            assert.deepEqual(await registration.getRegNums(), [{ reg_number: 'CA 745-564' }, { reg_number: 'CA 3443' }])
 
         });
     })
@@ -73,8 +95,8 @@ describe("Registration webapp", async function () {
             await registration.addRegNum("CL 228-384");
             await registration.addRegNum("CA 3534");
             await registration.addRegNum("CF 1234");
-        
-            assert.deepEqual( await registration.filteredRegNums("CL"), [{ id: 2, reg_number: "CL 228-384", town_id: 3 }],)
+
+            assert.deepEqual(await registration.filteredRegNums("CL"), [{ id: 2, reg_number: "CL 228-384", town_id: 3 }],)
 
 
         })
