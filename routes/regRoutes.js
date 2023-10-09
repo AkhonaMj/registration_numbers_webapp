d8677170f28075df624206f0b069343e0251e818
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-10-09 13:45:08 +0200

    -

--- routes/regRoutes.js
+++ routes/regRoutes.js
@@ -41,7 +41,6 @@ export default function RegRoutes(registrationInst, registrationDb) {
         }
         else if (registrationInst.validReg(regNum)) {
             await registrationDb.addRegNum(regNum)
-            // return req.flash('success', "Registration successfully added!")
         }
         res.redirect('/')
 
